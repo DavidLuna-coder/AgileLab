dc8e39d9c8755996eb3d0489bc4302ac013307bf
DCoder <david347lj@gmail.com>
2024-09-25 19:44:36 +0200

    Cambios en los paquetes y migración de proyectos

--- TFG/Domain/Entities/Project.cs
+++ TFG/Domain/Entities/Project.cs
@@ -3,6 +3,9 @@
     public class Project
     {
         public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; } = string.Empty;
         public ICollection<Notification> Notifications { get; set; }
+        public ICollection<User> Users { get; set; }
     }
 }
---
+++ TFG/Migrations/20240925174359_ProjectChanges.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TFG.Migrations
+{
+    /// <inheritdoc />
+    public partial class ProjectChanges : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Projects",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "Projects",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "ProjectId",
+                table: "AspNetUsers",
+                type: "uniqueidentifier",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetUsers_ProjectId",
+                table: "AspNetUsers",
+                column: "ProjectId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AspNetUsers_Projects_ProjectId",
+                table: "AspNetUsers",
+                column: "ProjectId",
+                principalTable: "Projects",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_AspNetUsers_Projects_ProjectId",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropIndex(
+                name: "IX_AspNetUsers_ProjectId",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Projects");
+
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "Projects");
+
+            migrationBuilder.DropColumn(
+                name: "ProjectId",
+                table: "AspNetUsers");
+        }
+    }
+}
