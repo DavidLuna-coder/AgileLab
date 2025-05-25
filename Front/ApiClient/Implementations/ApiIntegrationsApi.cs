e07b62d5c5a4f5dbedf56df039d2a6cc17dea276
DCoder <david347lj@gmail.com>
2025-05-25 17:32:19 +0200

    Se añade en el apiclient las integraciones de las apis

---
+++ Front/ApiClient/Implementations/ApiIntegrationsApi.cs
@@ -0,0 +1,20 @@
+﻿using Front.ApiClient.Interfaces;
+using Shared.DTOs.Integrations;
+
+namespace Front.ApiClient.Implementations
+{
+	public class ApiIntegrationsApi(IApiHttpClient client) : IApiIntegrationsApi
+	{
+		private const string API_INTEGRATION_ENDPOINT = "api/integrations";
+
+		public Task<List<IntegrationDto>> GetIntegrations()
+		{
+			return client.GetAsync<List<IntegrationDto>>(API_INTEGRATION_ENDPOINT);
+		}
+
+		public Task<IntegrationDto> UpdateApiIntegration(ApiConfigurationType type, UpdateApiIntegrationDto apiIntegrationDto)
+		{
+			return client.PutAsync<UpdateApiIntegrationDto,IntegrationDto>($"{API_INTEGRATION_ENDPOINT}/{(int)type}", apiIntegrationDto);
+		}
+	}
+}
---
+++ Front/ApiClient/Interfaces/IApiIntegrationsApi.cs
@@ -0,0 +1,10 @@
+﻿using Shared.DTOs.Integrations;
+
+namespace Front.ApiClient.Interfaces
+{
+	public interface IApiIntegrationsApi
+	{
+		Task<List<IntegrationDto>> GetIntegrations();
+		Task<IntegrationDto> UpdateApiIntegration(ApiConfigurationType type, UpdateApiIntegrationDto apiIntegrationDto);
+	}
+}
