20952ae724ea2fc6185e07f4e8c11b6a5549e2e7
DCoder <david347lj@gmail.com>
2025-05-25 16:16:26 +0200

    Seeding de datos de configuración

---
+++ TFG/Domain/Entities/ApiConfiguration.cs
@@ -0,0 +1,18 @@
+﻿namespace TFG.Domain.Entities
+{
+	public class ApiConfiguration
+	{
+		public int Id { get; set; }
+		public ApiConfigurationType Type { get; set; }
+		public string Name { get; set; }
+		public string BaseUrl { get; set; }
+		public string Token { get; set; }
+	}
+
+	public enum ApiConfigurationType
+	{
+		Gitlab = 1,
+		OpenProject = 2,
+		SonarQube = 3,
+	}
+}
--- TFG/Infrastructure/Data/ApplicationDbContext.cs
+++ TFG/Infrastructure/Data/ApplicationDbContext.cs
@@ -1,22 +1,113 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using TFG.Domain.Entities;
 using TFG.Model.Entities;
 
 namespace TFG.Infrastructure.Data
 {
-    public class ApplicationDbContext : IdentityDbContext<User>
-    {
-        public DbSet<User> Users { get; set; }
-        public DbSet<Project> Projects { get; set; }
-        public DbSet<Notification> Notifications { get; set; }
-        public DbSet<Rol> Roles { get; set; }
-        //public DbSet<Template> Templates { get; set; }
-        public ApplicationDbContext(DbContextOptions options) : base(options)
-        {
-        }
-        protected override void OnModelCreating(ModelBuilder builder)
-        {
-            base.OnModelCreating(builder);
-        }
-    }
+	public class ApplicationDbContext : IdentityDbContext<User>
+	{
+		public DbSet<User> Users { get; set; }
+		public DbSet<Project> Projects { get; set; }
+		public DbSet<Notification> Notifications { get; set; }
+		public DbSet<Rol> Roles { get; set; }
+		public DbSet<ApiConfiguration> ApiConfigurations { get; set; }
+		//public DbSet<Template> Templates { get; set; }
+		public ApplicationDbContext(DbContextOptions options) : base(options)
+		{
+		}
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			optionsBuilder
+				.UseSeeding((context, _) =>
+				{
+					SeedConfig(context);
+				})
+				.UseAsyncSeeding(async (context, _, cancellationToken) =>
+				{
+					await SeedConfigAsync(context);
+				});
+			base.OnConfiguring(optionsBuilder);
+		}
+
+		private void SeedConfig(DbContext context)
+		{
+			var gitlabConfig = context.Set<ApiConfiguration>().FirstOrDefault(c => c.Type == ApiConfigurationType.Gitlab);
+			if (gitlabConfig == null)
+			{
+				context.Set<ApiConfiguration>().Add(new ApiConfiguration
+				{
+					Type = ApiConfigurationType.Gitlab,
+					Name = "Gitlab",
+					BaseUrl = string.Empty,
+					Token = string.Empty
+				});
+			}
+
+			var openProjectConfig = context.Set<ApiConfiguration>().FirstOrDefault(c => c.Type == ApiConfigurationType.OpenProject);
+			if (openProjectConfig == null)
+			{
+				context.Set<ApiConfiguration>().Add(new ApiConfiguration
+				{
+					Type = ApiConfigurationType.OpenProject,
+					Name = "OpenProject",
+					BaseUrl = string.Empty,
+					Token = string.Empty
+				});
+			}
+
+			var sonarQubeConfig = context.Set<ApiConfiguration>().FirstOrDefault(c => c.Type == ApiConfigurationType.SonarQube);
+			if (sonarQubeConfig == null)
+			{
+				context.Set<ApiConfiguration>().Add(new ApiConfiguration
+				{
+					Type = ApiConfigurationType.SonarQube,
+					Name = "SonarQube",
+					BaseUrl = string.Empty,
+					Token = string.Empty
+				});
+			}
+			context.SaveChanges();
+		}
+
+		private async Task SeedConfigAsync(DbContext context)
+		{
+			var gitlabConfig = await context.Set<ApiConfiguration>().FirstOrDefaultAsync(c => c.Type == ApiConfigurationType.Gitlab);
+			if (gitlabConfig == null)
+			{
+				context.Set<ApiConfiguration>().Add(new ApiConfiguration
+				{
+					Type = ApiConfigurationType.Gitlab,
+					Name = "Gitlab",
+					BaseUrl = string.Empty,
+					Token = string.Empty
+				});
+			}
+
+			var openProjectConfig = context.Set<ApiConfiguration>().FirstOrDefaultAsync(c => c.Type == ApiConfigurationType.OpenProject);
+			if (openProjectConfig == null)
+			{
+				context.Set<ApiConfiguration>().Add(new ApiConfiguration
+				{
+					Type = ApiConfigurationType.OpenProject,
+					Name = "OpenProject",
+					BaseUrl = string.Empty,
+					Token = string.Empty
+				});
+			}
+
+			var sonarQubeConfig = context.Set<ApiConfiguration>().FirstOrDefaultAsync(c => c.Type == ApiConfigurationType.SonarQube);
+			if (sonarQubeConfig == null)
+			{
+				context.Set<ApiConfiguration>().Add(new ApiConfiguration
+				{
+					Type = ApiConfigurationType.SonarQube,
+					Name = "SonarQube",
+					BaseUrl = string.Empty,
+					Token = string.Empty
+				});
+			}
+			await context.SaveChangesAsync();
+		}
+	}
 }
