9431180890d9e9e5103b414787f31474c4836951
DCoder <david347lj@gmail.com>
2025-05-28 19:09:22 +0200

    Actualizado aspire

--- TFG.Aspire/Program.cs
+++ TFG.Aspire/Program.cs
@@ -4,6 +4,6 @@ var builder = DistributedApplication.CreateBuilder(args);
 var sql = builder.AddSqlServer("localdb");
 var sqldb = sql.AddDatabase("db");
 
-var api = builder.AddProject<Projects.TFG>("tfgapi").WithReference(sqldb);
+var api = builder.AddProject<Projects.TFG>("tfgapi");
 
 builder.Build().Run();
